89bf6b12c7282c9bd98c7c3450574f37fb5816da
David James Humphreys <davidjameshumphreys@gmail.com>
2025-05-03 20:31:08 +0100

    Days in the year so far

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,23 @@
--- A text input for reversing text. Very useful!
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/text_fields.html
---
-
-
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Attribute, Html, div, input, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-
-
+import Task
+import Time exposing (Month(..), Posix, Zone)
+import Time exposing (Month(..), Posix, Zone, monthToNumber, posixToMillis)
 
 -- MAIN
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -26,13 +25,16 @@ main =
 
 
 type alias Model =
-    { content : String
+    { zone : Time.Zone
+    , time : Time.Posix
     }
 
 
-init : Model
-init =
-    { content = "" }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Time.utc (Time.millisToPosix 0)
+    , Task.perform AdjustTimeZone Time.here
+    )
 
 
 
@@ -40,23 +42,168 @@ init =
 
 
 type Msg
-    = Change String
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Change newContent ->
-            { model | content = newContent }
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
+
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
 
 
 
 -- VIEW
 
+{-| Calculate the day of the year (1-366) for a given date
+-}
+dayOfYear : Zone -> Posix -> Int
+dayOfYear zone time =
+    let
+        year =
+            Time.toYear zone time
+
+        month =
+            Time.toMonth zone time
+
+        day =
+            Time.toDay zone time
+
+        isLeap =
+            (modBy 4 year == 0) && ((modBy 100 year /= 0) || (modBy 400 year == 0))
+
+        -- Get cumulative days at the start of each month
+        daysBeforeMonth =
+            cumulativeDaysByMonth isLeap month
+    in
+    daysBeforeMonth + day
+
+{-| Cumulative days at the start of each month (days before the first day of the month)
+-}
+cumulativeDaysByMonth : Bool -> Month -> Int
+cumulativeDaysByMonth isLeap month =
+    let
+        feb = if isLeap then 29 else 28
+    in
+    case month of
+        Jan -> 0
+        Feb -> 31
+        Mar -> 31 + feb
+        Apr -> 31 + feb + 31
+        May -> 31 + feb + 31 + 30
+        Jun -> 31 + feb + 31 + 30 + 31
+        Jul -> 31 + feb + 31 + 30 + 31 + 30
+        Aug -> 31 + feb + 31 + 30 + 31 + 30 + 31
+        Sep -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31
+        Oct -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30
+        Nov -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
+        Dec -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30
+
+
+
+
+dayOfYear : Time.Month -> Int -> Int
+dayOfYear month day =
+    (case month
+
+    ) + day
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
-        , div [] [ text (String.reverse model.content) ]
-        ]
+    let
+        hour =
+            String.fromInt (Time.toHour model.zone model.time)
+
+        minute =
+            String.fromInt (Time.toMinute model.zone model.time)
+
+        second =
+            String.fromInt (Time.toSecond model.zone model.time)
+
+        day =
+            String.fromInt (Time.toDay model.zone model.time)
+    in
+    h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second ++ ", " ++ day) ]
+
+
+{-| Calculate the day of the year (1-366) for a given date
+-}
+dayOfYear : Zone -> Posix -> Int
+dayOfYear zone time =
+    let
+        year =
+            Time.toYear zone time
+
+        month =
+            Time.toMonth zone time
+
+        day =
+            Time.toDay zone time
+
+        isLeap =
+            (modBy 4 year == 0) && ((modBy 100 year /= 0) || (modBy 400 year == 0))
+
+        -- Get cumulative days at the start of each month
+        daysBeforeMonth =
+            cumulativeDaysByMonth isLeap month
+    in
+    daysBeforeMonth + day
+
+
+{-| Days in each month
+-}
+daysInMonth : Month -> Bool -> Int
+daysInMonth month isLeap =
+    case month of
+        Jan -> 31
+        Feb -> if isLeap then 29 else 28
+        Mar -> 31
+        Apr -> 30
+        May -> 31
+        Jun -> 30
+        Jul -> 31
+        Aug -> 31
+        Sep -> 30
+        Oct -> 31
+        Nov -> 30
+        Dec -> 31
+
+
+{-| Cumulative days at the start of each month (days before the first day of the month)
+-}
+cumulativeDaysByMonth : Bool -> Month -> Int
+cumulativeDaysByMonth isLeap month =
+    let
+        feb = if isLeap then 29 else 28
+    in
+    case month of
+        Jan -> 0
+        Feb -> 31
+        Mar -> 31 + feb
+        Apr -> 31 + feb + 31
+        May -> 31 + feb + 31 + 30
+        Jun -> 31 + feb + 31 + 30 + 31
+        Jul -> 31 + feb + 31 + 30 + 31 + 30
+        Aug -> 31 + feb + 31 + 30 + 31 + 30 + 31
+        Sep -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31
+        Oct -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30
+        Nov -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
+        Dec -> 31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30
+        --     Jan  Feb   Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov
